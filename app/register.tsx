1505e96aa8979e4f126bafbb99d5abe7dac0c482
abuk10252-jpg <abuk10252@gmail.com>
2026-04-27 18:53:42 +0200

    Update register.tsx

--- app/register.tsx
+++ app/register.tsx
@@ -9,8 +9,8 @@ import {
   Alert,
   ScrollView,
 } from 'react-native';
-import { useAuth } from '../context/AuthContext';
-import { registerUser } from '../utils/api';
+import { useAuth } from '../../src/context/AuthContext';
+import { registerUser } from '../../src/utils/api';
 
 export default function RegisterScreen({ navigation }: any) {
   const { setUser, setToken } = useAuth();
