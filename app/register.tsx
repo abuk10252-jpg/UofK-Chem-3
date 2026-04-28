70dbfcf7e642480340740f55ae6bf468c2dcddc8
abuk10252-jpg <abuk10252@gmail.com>
2026-04-28 06:34:29 +0200

    Update register.tsx

--- app/register.tsx
+++ app/register.tsx
@@ -2,4 +2,5 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform, ScrollView, ActivityIndicator } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import { useAuth } from '../src/context/
+import { useAuth } from '../src/context/AuthContext';
+
